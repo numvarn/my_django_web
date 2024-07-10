f1af61af404032eb33bd3f8fe63ab4111fa4c582
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2024-07-10 09:14:15 +0700

    setup page

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.indexPage),
+    path("about/", views.aboutPage)
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def indexPage(request):
     return render(request, "index.html")
+
+
+def aboutPage(request):
+    return render(request, "about.html")
