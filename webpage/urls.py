e4ac0218a8967ef429f7c012b5c4253697b8dd1e
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2024-08-14 09:20:03 +0700

    post method

--- webpage/urls.py
+++ webpage/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("about/", views.aboutPage, name="about"),
     path("for/", views.forPage, name='for-page'),
     path("color/", views.cardColorPage, name='color-page'),
+    path("form/", views.formPage, name='form-page'),
 ]
